90e53cf874cfb5fb05612e1de8ed913b4c9144bc
Lio <liocicolecchia@gmail.com>
2026-07-17 16:50:13 +0200

    adding a user interaction to the component. <p> text changes on click of a button. writing to tests for it, text before click and a second test where the button gets clicked and it tests if the text changes

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  function changeTextHandler() {
+    setChangedText(true);
+  }
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It0s good to see you!</p>
+      {changedText ? <p>Changed!</p> : <p>It's good to see you!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -12,5 +13,29 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was Not clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("It's good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders changed if the button was clicked", async () => {
+    // Arange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    await userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("changed", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
   });
 });
